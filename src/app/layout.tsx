a61eb079ad52d35af91df0280912a8fc3b61c57d
Thiago Navarro <dollynhoextremeyt@gmail.com>
2025-04-22 23:03:03 -0300

    theme changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,13 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@mui/material/styles"; // Importando o ThemeProvider do MUI
 import theme from "./theme/theme";  // Importando o tema que você criou
+// import { Mulish } from 'next/font/google'
+
+// const mulish = Mulish({
+//   subsets: ['latin'],
+//   weight: ['300', '400', '600', '700', '800'],
+//   variable: '--font-mulish',
+// })
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,6 +22,11 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+// export const metadata = {
+//   title: 'Okay, Where and When?',
+//   description: '...',
+// }
 
 export default function RootLayout({
   children,
