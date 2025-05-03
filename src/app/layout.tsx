2a4df67cf471ed954e7587e246598ccd30a83ba6
Thiago Navarro <dollynhoextremeyt@gmail.com>
2025-05-03 00:24:29 -0300

    reorganização da layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,43 +1,19 @@
-// src/app/layout.tsx
-
-'use client';  // Isso indica que esse componente é do lado do cliente
+'use client';
 
 import { Geist, Geist_Mono } from "next/font/google";
-import { ThemeProvider } from "@mui/material/styles"; // Importando o ThemeProvider do MUI
-import theme from "./theme/theme";  // Importando o tema que você criou
-// import { Mulish } from 'next/font/google'
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme/theme";
 
-// const mulish = Mulish({
-//   subsets: ['latin'],
-//   weight: ['300', '400', '600', '700', '800'],
-//   variable: '--font-mulish',
-// })
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
+const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-// export const metadata = {
-//   title: 'Okay, Where and When?',
-//   description: '...',
-// }
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ThemeProvider theme={theme}>
-          {children}
+            {children}
         </ThemeProvider>
       </body>
     </html>
